9a9865b8c0766d3fc0282bd87e4caa620a0cb42a
Maail Anwar <maailanwaralig@gmail.com>
2023-06-30 20:40:55 +0530

    Backend Added

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+const express  = require("express");
+const mongoose = require("mongoose");
+const cors =require("cors");
+const app = express();
+const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000","http://map-pinning-app.onrender.com"]
+}));
+
+mongoose.connect(process.env.URL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>{
+    console.log("mongodb connected");
+}).catch((err)=>{
+    console.log("mongodb not connected")
+});
+app.use("/api/pins",pinRoute);
+app.use("/api/users",userRoute);
+
+app.listen(process.env.PORT,()=>{
+    console.log("backend server is running!!")
+})
